cbd0db96dfddaa3ebf1b90f98dc41d908cf89c28
William Sargent <wns1997@icloud.com>
2021-08-02 22:25:33 -0400

    Made descriptions clear
    I made the descriptive text clearer.

--- WeatherPy/config.py
+++ WeatherPy/config.py
@@ -1,5 +1,5 @@
-# OpenWeatherMap API Key
+# This is the OpenWeatherMap API Key
 weather_api_key = "cd14ce77c1208bc2e5a14799cde74460"
 
-# Google API Key
+# This is the Google API Key
 g_key = "YOUR KEY HERE!"
